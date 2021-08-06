0d5f84e69f4042c52355b824dfada2d933654414
Jay Lee <jay.lee.2280@gmail.com>
2021-08-06 22:18:09 +0900

    JS: Login

--- src/login.js
+++ src/login.js
@@ -4,10 +4,13 @@ const loginInput = document.querySelector('#login-form input');
 const title = document.querySelector('title');
 const planner_name = document.querySelector('#planner_name');
 
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
 function handleClickBtn() {
   const check = confirm('Do you want to set/reset your name?');
   if (check === true) {
-    loginForm.classList.remove('hidden');
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
   }
 }
 
@@ -16,17 +19,17 @@ rename.addEventListener('click', handleClickBtn);
 function resetName(event) {
   event.preventDefault();
   const username = loginInput.value;
-  localStorage.setItem('username', username);
+  localStorage.setItem(USERNAME_KEY, username);
   paintUsername(username);
 }
 
 function paintUsername(username) {
   title.innerText = `${username}'s Planner`;
   planner_name.innerText = `${username}'s Planner`;
-  loginForm.classList.add('hidden');
+  loginForm.classList.add(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem('username');
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername !== null) {
   paintUsername(savedUsername);
 }
