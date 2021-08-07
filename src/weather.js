628ec65574120035c31747a666e95573b03ac8e0
Jay Lee <jay.lee.2280@gmail.com>
2021-08-07 21:39:58 +0900

    Weather

---
+++ src/weather.js
@@ -0,0 +1,27 @@
+const weatherBtn = document.querySelector('button.get_weather');
+const weather = document.querySelector('.weather-box p:last-child');
+
+const API_KEY = '7e26b7437dae1176e459d8f7f448efae';
+
+function geoSuccess(position) {
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = data.name;
+      const mainWeather = data.weather[0].main;
+      const temperature = Math.round(data.main.temp);
+      const humidity = data.main.humidity;
+      weather.innerText = `${city} / ${mainWeather} / ${temperature}℃ / humidity ${humidity}`;
+    });
+}
+
+navigator.geolocation.getCurrentPosition(geoSuccess);
+
+function paintWeather() {
+  console.log('hi');
+}
+
+weatherBtn.addEventListener('click', paintWeather);
