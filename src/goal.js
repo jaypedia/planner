816390228475a7571bc7a4e201ec355d3fd9a833
Jay Lee <jay.lee.2280@gmail.com>
2021-08-09 16:46:20 +0900

    JS:Goal upgrade

--- src/goal.js
+++ src/goal.js
@@ -3,19 +3,22 @@ const goalForm = document.querySelector('.my-plans form');
 const goalInput = document.querySelector('.my-plans form input');
 const resetBtn = document.querySelector('.my-plans button');
 
+const USERGOAL_KEY = 'userGoal';
+const HIDDEN_CLASSNAME = 'hidden';
+
 function setGoal(event) {
   event.preventDefault();
   const userGoal = goalInput.value;
-  localStorage.setItem('userGoal', userGoal);
+  localStorage.setItem(USERGOAL_KEY, userGoal);
   paintGoal(userGoal);
 }
 
-const savedGoal = localStorage.getItem('userGoal');
+const savedGoal = localStorage.getItem(USERGOAL_KEY);
 
 function paintGoal(userGoal) {
   goal.innerText = userGoal;
-  goalForm.classList.add('hidden');
-  resetBtn.classList.remove('hidden');
+  goalForm.classList.add(HIDDEN_CLASSNAME);
+  resetBtn.classList.remove(HIDDEN_CLASSNAME);
 }
 
 goalForm.addEventListener('submit', setGoal);
@@ -23,15 +26,16 @@ goalForm.addEventListener('submit', setGoal);
 if (savedGoal !== null && savedGoal !== '') {
   paintGoal(savedGoal);
 } else {
-  goalForm.classList.remove('hidden');
+  goalForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function resetGoal() {
   const answer = confirm('Do you want to reset your goal?');
   if (answer === true) {
-    goalForm.classList.remove('hidden');
+    goalForm.classList.remove(HIDDEN_CLASSNAME);
     goal.innerText = '';
-    resetBtn.classList.add('hidden');
+    resetBtn.classList.add(HIDDEN_CLASSNAME);
+    localStorage.removeItem(USERGOAL_KEY);
   }
 }
 
